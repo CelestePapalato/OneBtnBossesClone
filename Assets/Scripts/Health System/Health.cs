b0f76c36e71e04a780acf9d72ae96058a8c4633c
videojuegoslagos <160129157+videojuegoslagos@users.noreply.github.com>
2024-10-29 20:26:29 -0300

    Implementación de un Sistema de Vida para el Jugador
    Modificado Health para que cumpla con las necesidades del Jugador.
    - Se añadió una lógica de invencibilidad por un tiempo definido.

--- Assets/Scripts/Health System/Health.cs
+++ Assets/Scripts/Health System/Health.cs
@@ -7,8 +7,12 @@ public class Health : MonoBehaviour
 {
     [SerializeField]
     private int maxHealth;
+    [SerializeField]
+    private float invincibilityTimer;
 
     private int health;
+    private bool invincible = false;
+    Collider2D hurtbox;
 
     public UnityAction<int, int> OnDamaged;
     public UnityAction OnDeath;
@@ -16,20 +20,33 @@ public class Health : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
+        hurtbox = GetComponent<Collider2D>();
     }
 
     private void Damage()
     {
+        if(invincible) { return; }
         health = Mathf.Max(health-1, 0);
         OnDamaged?.Invoke(health, maxHealth);
         if(health <= 0)
         {
             OnDeath?.Invoke();
         }
+        StartCoroutine(InvincibilityEnabler());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Damage();
     }
+
+    private IEnumerator InvincibilityEnabler()
+    {
+        hurtbox.enabled = false;
+        invincible = true;
+        yield return new WaitForSeconds(invincibilityTimer);
+        hurtbox.enabled = true;
+        invincible = false;
+    }
+
 }
