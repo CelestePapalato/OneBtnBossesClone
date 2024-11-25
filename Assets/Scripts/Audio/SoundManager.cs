2015f59b7f7842cd58d59b874fd0c05373977331
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-11-25 11:40:12 -0300

    Añadido Sound Manager

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -4,13 +4,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
-{
-    [Serializable]
-    private class AudioClipData
+{    public static SoundManager Instance { get; private set; }
+
+    [SerializeField]
+    AudioSource BGMAudioSource;
+    [SerializeField]
+    AudioList SEAudioList;
+    [SerializeField]
+    AudioList BGMAudioList;
+
+    private void Awake()
     {
-        public string tag;
-        public AudioClip audioClip;
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
+    public void PlayBGM(string audioClip, bool loop)
+    {
+        if (!BGMAudioList || !BGMAudioSource) { return; }
 
+        AudioClip toPlay;
+        if (!BGMAudioList.TryGetAudio(audioClip, out toPlay))
+        {
+            return;
+        }
+        BGMAudioSource.Stop();
+        BGMAudioSource.clip = toPlay;
+        BGMAudioSource.loop = loop;
+        BGMAudioSource.Play();
+    }
+
+    public void BGMStop()
+    {
+        BGMAudioSource.Stop();
+    }
+
+    public void PlaySE(string audioClip, Vector2 point)
+    {
+        if (!SEAudioList) { return; }
+        AudioClip toPlay;
+        if(!SEAudioList.TryGetAudio(audioClip, out toPlay)) { return; }
+        AudioSource.PlayClipAtPoint(toPlay, point);
+    }
 }
